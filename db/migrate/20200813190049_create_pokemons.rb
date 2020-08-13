c116d75ac1ef3191e1f7ddb0e8304677f5d3cdd5
ClaFiore <claborghini@gmail.com>
2020-08-13 15:11:26 -0400

    set up pokemon and type

---
+++ db/migrate/20200813190049_create_pokemons.rb
@@ -0,0 +1,19 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.integer :trainer_id
+      t.integer :type_id
+      t.string :species
+      t.string :nickname
+      t.string :pokedex_entry
+      t.integer :speed
+      t.integer :defence
+      t.integer :attack
+      t.integer :max_hp
+      t.integer :current_hp
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
